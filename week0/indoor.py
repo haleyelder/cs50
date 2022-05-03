75bcbf02ada7c857956c74fac2779ba12e338218
haleyelder <haley.elder@live.com>
2022-05-02 21:58:05 -0700

    add indoor, playback problems

---
+++ week0/indoor.py
@@ -0,0 +1,8 @@
+# TEST CASES
+# - HELLO > hello
+# - THIS IS CS50 > this is cs50
+# - 50 > 50
+
+text = input()
+text = text.lower()
+print(text)
---
+++ week0/playback.py
@@ -0,0 +1,8 @@
+# # TEST CASES
+# - This is CS50 > This...is...CS50
+# - This is our week on functions > This...is...our...week...on...functions
+# - Let's implement a function called hello > Let's...implement...a...function...called...hello
+
+text = input()
+text = text.replace(" ","...")
+print(text)
